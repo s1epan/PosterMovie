94af63747b4df21e865968204a1ec5ef5dc9a427
Stepan <anergaryan@mail.ru>
2024-03-31 19:54:28 +0300

    сделал запрос получения фильмов API

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Content from "./components/Content/Content";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios({
+      url: "https://www.omdbapi.com/?t=iron&apikey=e9895de2",
+    }).then((res) => setData(res.data));
+  }, []);
+
   return (
     <>
       <Header />
