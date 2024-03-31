07c9bb3a6d3d782387637408a72215c0950e35ff
Stepan <anergaryan@mail.ru>
2024-03-31 19:36:17 +0300

    создал компонент content, добавил background картинку на body

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Content from "./components/Content/Content";
 
 function App() {
   return (
     <>
       <Header />
+      <Content />
       <Footer />
     </>
   );
